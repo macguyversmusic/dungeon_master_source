c00e9d233d8327e58b84d371f4279fc2c992309e
JeroenDerks <jeroenderks88@gmail.com>
2023-05-11 11:40:23 +0200

    feat: add stuarts tests

--- frontend/utils/threeConfig.ts
+++ frontend/utils/threeConfig.ts
@@ -15,21 +15,12 @@ import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 import { PAGE_PADDING } from "./constants";
 
-import { USDZLoader } from "three-usdz-loader";
-import { USDZInstance } from "three-usdz-loader/lib/USDZInstance";
-
 export let mixer: any;
-// Setup the USDZ loader
-// You may place external dependencies (.wasm / worker files) in a subfolder of the public folder.
-// In this case, pass the path of this folder as an arg to the USDZLoader constructor
-export let loader = new USDZLoader("/wasm");
-export const scene = new THREE.Scene();
 
-export async function initThree() {
+export function initThree() {
   const clock = new THREE.Clock();
 
-  // Create a ThreeJs Group in which the loaded USDZ model will be placed
-  // Add the group to the scene
+  const container = document.getElementById("skecth-container");
 
   const camera = new THREE.PerspectiveCamera(
     45,
@@ -37,10 +28,9 @@ export async function initThree() {
     1,
     20
   );
-  camera.position.set(-1, 1, 2.5);
+  camera.position.set(-10, 2.5, 2.5);
 
-  const container = document.getElementById("skecth-container");
-  // document.body.appendChild(container);
+  const scene = new THREE.Scene();
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -53,50 +43,84 @@ export async function initThree() {
 
   container?.appendChild(renderer.domElement);
 
-  // console.log(loadedModel.getGroup());
-  // const light = new THREE.PointLight(0xff0000, 5, 10);
-  // light.position.set(0, -3, 4);
-  // scene.add(light);
-
-  // scene.add(group);
+  const light = new THREE.PointLight(0xff0000, 5, 10);
+  light.position.set(-10, -3, 4);
+  scene.add(light);
 
   const ktx2Loader = new KTX2Loader()
     .setTranscoderPath("/textures/basis/")
     .detectSupport(renderer);
 
-  new GLTFLoader()
-    .setKTX2Loader(ktx2Loader)
-    .setMeshoptDecoder(MeshoptDecoder)
-    .load("/facecap.glb", (gltf: any) => {
-      console.log("gltf", gltf);
+    new GLTFLoader()
+  .setKTX2Loader(ktx2Loader)
+  .setMeshoptDecoder(MeshoptDecoder)
+  .load("/mocap.glb", (gltf) => {
+    console.log("gltf", gltf);
 
-      const mesh = gltf.scene.children[0];
-      scene.add(mesh);
-      console.log("mesh", mesh);
+    const mesh = gltf.scene.children[0];
+    scene.add(mesh);
+    console.log("mesh", mesh);
 
-      mixer = new THREE.AnimationMixer(mesh);
-      const zeroTrack = new THREE.NumberKeyframeTrack(
-        "mesh_2.morphTargetInfluences",
-        [0, 1],
-        Array.from({ length: 104 }, () => 0)
-      );
-      let zeroClip = new THREE.AnimationClip("blink", -1, [zeroTrack]);
-      mixer.clipAction(zeroClip).play();
+    let morphTargetMeshes = [];
 
-      const head = mesh.getObjectByName("mesh_2");
-      const influences = head?.morphTargetInfluences;
-
+    gltf.scene.traverse((node) => {
+      if (node.isMesh && node.name.startsWith("LOD0_3_")) {
+        morphTargetMeshes.push(node);
+        console.log(node.name)
+      }
+    });
+    
+    for (const morphTargetMesh of morphTargetMeshes) {
+      const influences = morphTargetMesh.morphTargetInfluences;
       const gui = new GUI();
       gui.close();
-
-      for (const [key, value] of Object.entries(head.morphTargetDictionary)) {
+    
+      for (const [key, value] of Object.entries(morphTargetMesh.morphTargetDictionary)) {
         console.log(key, value);
         gui
           .add(influences, value, 0, 1, 0.01)
           .name(key.replace("blendShape1.", ""))
           .listen(influences);
       }
-    });
+    }
+    
+  });
+
+
+  // new GLTFLoader()
+  //   .setKTX2Loader(ktx2Loader)
+  //   .setMeshoptDecoder(MeshoptDecoder)
+  //   .load("/scene.glb", (gltf: any) => {
+  //     console.log("gltf", gltf);
+
+  //     const mesh = gltf.scene.children[0];
+  //     scene.add(mesh);
+  //     console.log("mesh", mesh);
+  //     mixer = new THREE.AnimationMixer(mesh);
+
+  //     // mixer.clipAction(zeroClip).play();
+  //     // GUI
+
+  //     let morphTargetMesh;
+  //     gltf.scene.traverse((node) => {
+  //       if (node.isMesh && node.name.startsWith("LOD0_3_")) {
+  //         morphTargetMesh = node;
+  //       }
+  //     });
+      
+  //           const head = morphTargetMesh;
+  //     const influences = head.morphTargetInfluences;
+  //     const gui = new GUI();
+  //     gui.close();
+
+  //     for (const [key, value] of Object.entries(head.morphTargetDictionary)) {
+  //       console.log(key, value);
+  //       gui
+  //         .add(influences, value, 0, 1, 0.01)
+  //         .name(key.replace("blendShape1.", ""))
+  //         .listen(influences);
+  //     }
+  //   });
 
   const environment = new RoomEnvironment();
   const pmremGenerator = new THREE.PMREMGenerator(renderer);
@@ -107,11 +131,11 @@ export async function initThree() {
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
   controls.minDistance = 2.5;
-  controls.maxDistance = 100;
+  controls.maxDistance = 5;
   controls.minAzimuthAngle = -Math.PI / 2;
   controls.maxAzimuthAngle = Math.PI / 2;
   controls.maxPolarAngle = Math.PI / 1.8;
-  // controls.target.set(0, 0.15, -0.2);
+  controls.target.set(0, 0.15, -0.2);
 
   // const stats = new Stats();
   // container.appendChild(stats.dom);
